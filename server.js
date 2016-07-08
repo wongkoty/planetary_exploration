1aa4455b6c8b01c3f65009decc6794ca49f7630f
Koty Wong <kotywong@Kotys-MacBook-Pro.local>
2016-07-08 09:56:27 -0400

    adding more planets

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+      app = express(),
+      logger = require('morgan'),
+      port = process.env.PORT || 5000;
+
+// middleware
+app.use(logger('dev'))
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+app.listen(port, function() {
+  console.log('we on port ' + port)
+})
+
+
+
+
+
