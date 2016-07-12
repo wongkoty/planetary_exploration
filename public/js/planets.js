c8fe24fec7125b790a0bc2ffb5fb97f9a4b16a8a
Koty Wong <kotywong@Kotys-MacBook-Pro.local>
2016-07-12 11:21:57 -0400

    adding axial tilt

---
+++ public/js/planets.js
@@ -0,0 +1,28 @@
+var mercury = {
+  name: "Mercury"
+}
+var venus = {
+  name: "Venus"
+}
+var earth = {
+  name: "Earth"
+}
+var mars = {
+  name: "Mars"
+}
+var jupiter = {
+  name: "Jupiter"
+}
+var saturn = {
+  name: "Saturn"
+}
+var uranus = {
+  name: "Uranus"
+}
+var neptune = {
+  name: "Neptune"
+}
+var Pluto = {
+  name: "Pluto"
+}
+
